6625896caad165695c305e9584e072f13c15168d
raSel <mdraselm413@gmail.com>
2022-10-17 23:55:03 +0600

    lavel

---
+++ src/_auth.steps.js
@@ -0,0 +1,9 @@
+/*
+Auth Setup
+1. create firebase project
+2. enable web
+3. enable signin method
+4. install firebase
+5. get firebase config in your firebase.config.js
+6. export app from firebase.config.js file 
+ */
---
+++ src/components/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name="email" id="" placeholder='Your Email Address' />
+                </div>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
