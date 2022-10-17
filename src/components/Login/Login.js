6d31806acc619c80551e330de30c8d17d03172ca
raSel <mdraselm413@gmail.com>
2022-10-18 00:36:51 +0600

    btn-add

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Login.css'
 
 const Login = () => {
@@ -8,9 +9,15 @@ const Login = () => {
             <form>
                 <div className="form-control">
                     <label htmlFor="email">Email</label>
-                    <input type="email" name="email" id="" placeholder='Your Email Address' />
+                    <input type="email" name="email" id="" placeholder='Your Email Address' required />
                 </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="" placeholder='Your Password' required />
+                </div>
+            <input className='btn-submit' type="submit" value="Login" />
             </form>
+            <p>New to Ema John? <Link to='/signup'>Create New Account</Link></p>
         </div>
     );
 };
